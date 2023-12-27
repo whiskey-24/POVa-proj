de347037dbc7a6bb7b7840fbb46e03573fd00d77
whiskey <matej.email01@gmail.com>
2023-12-27 20:20:26 +0100

    Changed network to resnet

--- detector/try_retinaface.py
+++ detector/try_retinaface.py
@@ -1,5 +1,5 @@
-import sys
 import cv2
+import sys
 sys.path.append(f"retinaface/Pytorch_Retinaface")
 from retinaface.Pytorch_Retinaface.detect import VehicleDetector
 
@@ -11,18 +11,20 @@ def mouse_callback(event, x, y, flags, param):
 
 video_path = "/home/whiskey/Documents/2Mit/POVa/POVa-proj/map_reg/point_reg/2023_08_10_14_40_06.mp4"
 
-vehicle_detector = VehicleDetector("mobile0.25", "retinaface/Pytorch_Retinaface/weights/mobilenet0.25_Final.pth",
+# vehicle_detector = VehicleDetector("mobile0.25", "retinaface/Pytorch_Retinaface/weights/mobilenet0.25_Final.pth",
+#                                    False, 0.5, True)
+vehicle_detector = VehicleDetector("resnet50", "retinaface/Pytorch_Retinaface/weights/Resnet50_Final.pth",
                                    False, 0.5, True)
 
 cv2.namedWindow("test", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("test", 640, 480)
 cv2.setMouseCallback("test", mouse_callback)
 
-# cap = cv2.VideoCapture(video_path)
-# ret, frame = cap.read()
-#
-# bbox = [(438, 60), (782, 338)]
-#
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+
+bbox = [(438, 60), (782, 338)]
+
 # paused = False
 # first_frame = True
 # frame_num = 0
@@ -33,7 +35,7 @@ cv2.setMouseCallback("test", mouse_callback)
 #             break
 #         frame_num += 1
 #         # Crop frame to bbox
-#         # frame = frame[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
+#         frame = frame[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
 #         if first_frame:
 #             first_frame = False
 #             paused = True
@@ -49,11 +51,11 @@ cv2.setMouseCallback("test", mouse_callback)
 #     elif key == ord(" "):
 #         paused = not paused
 
-# img_path = "/home/whiskey/Documents/2Mit/POVa/POVa-proj/detector/data/retinaface/images/00002_1_0.jpg"
-# img = cv2.imread(img_path)
-# vehicles, draw_img = vehicle_detector.detect_image(img)
-# cv2.imshow("test", draw_img)
-# while True:
-#     key = cv2.waitKey(1)
-#     if key == ord('q'):
-#         break
+img_path = "/home/whiskey/Documents/2Mit/POVa/POVa-proj/detector/data/retinaface/images/00002_1_0.jpg"
+img = cv2.imread(img_path)
+vehicles, draw_img = vehicle_detector.detect_image(img)
+cv2.imshow("test", draw_img)
+while True:
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
